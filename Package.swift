0a28a5a9e8a6ca296255eb96db1d056241a7b09e
Han <286116014@qq.com>
2026-06-30 13:49:02 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "AgentKit",
     platforms: [
         .iOS(.v18),
-        .macOS(.v26)
+        .macOS(.v15)
     ],
     products: [
         .library(
